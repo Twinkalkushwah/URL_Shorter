ee90f6b4c48fe5c9ae2677f861f2e527710b12af
unknown <twinkalkushwah01@gmail.com>
2025-04-02 13:50:53 +0530

    some changes

--- Server/routes/url.js
+++ Server/routes/url.js
@@ -34,33 +34,4 @@ urlRouter.post("/shorten", async (req, res) => {
   }
 });
 
-// Add this DELETE route to your urlRouter
-urlRouter.delete("/delete/:shortUrl", async (req, res) => {
-  try {
-    const { shortUrl } = req.params;
-    
-    // Find the URL by shortUrl and delete it
-    const deletedUrl = await Url.findOneAndDelete({ shortUrl });
-
-    if (!deletedUrl) {
-      return res.status(404).json({ success: false, message: "URL not found" });
-    }
-
-    res.status(200).json({ success: true, message: "URL removed successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: "Internal server error" });
-  }
-});
-
-
-urlRouter.get("/history", auth, async (req, res) => {
-  try {
-    const history = await Url.find({ user: req.user._id });
-    res.status(200).json({ message: "History", history, isOk: true });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error", isOk: false });
-  }
-});
-
 module.exports = { urlRouter };
